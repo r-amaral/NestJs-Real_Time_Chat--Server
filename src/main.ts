062158eea475d4e4fa37dede11fcaf1be79cd715
Ruan Oliveira <ruan.oliveira_compasso@terrainvestimentos.com.br>
2023-05-31 19:24:50 -0300

    feat: definindo cors para websockets no initServer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function initServer() {
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
+  app.enableCors();
+  await app.listen(3001);
 }
-bootstrap();
+initServer();
