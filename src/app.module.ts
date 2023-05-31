ab8832eff592fed3a7f26b9cd9a153010218f582
Ruan Oliveira <ruan.oliveira_compasso@terrainvestimentos.com.br>
2023-05-31 19:36:40 -0300

    feat: adicionando gateway ao ao provider de modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from "@nestjs/config";
+import { ScheduleModule } from "@nestjs/schedule";
+import { Module } from "@nestjs/common";
+import { AppGateway } from "./server/app.gateway";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), ScheduleModule.forRoot()],
+  providers: [AppGateway],
 })
 export class AppModule {}
