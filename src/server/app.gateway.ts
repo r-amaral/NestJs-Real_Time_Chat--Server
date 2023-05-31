ba013fe787a70d0261864f6b84aa994c56d365ad
Ruan Oliveira <ruan.oliveira_compasso@terrainvestimentos.com.br>
2023-05-31 19:32:17 -0300

    feat: Criando class para tratar websocket

---
+++ src/server/app.gateway.ts
@@ -0,0 +1,22 @@
+import { Logger } from "@nestjs/common";
+import { Server, Socket } from "socket.io";
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+} from "@nestjs/websockets";
+
+@WebSocketGateway({
+  cors: "*",
+})
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
+
+  private logger = new Logger(AppGateway.name);
+}
