a725d60d1ab73782782348fd567d06a00e3a6f40
Ruan Oliveira <ruan.oliveira_compasso@terrainvestimentos.com.br>
2023-05-31 19:35:22 -0300

    feat: Criando subscribes para retornar mensagem ao usuário logado e para informar quando usuário entrar na sala

--- src/server/app.gateway.ts
+++ src/server/app.gateway.ts
@@ -31,4 +31,18 @@ export class AppGateway implements OnGatewayInit, OnGatewayDisconnect {
       message: "all messages",
     });
   }
+
+  @SubscribeMessage("msgToClient")
+  handleMessageChat(_: Socket, payload: any): void {
+    this.server.emit("msgToClient", payload);
+    this.logger.log(
+      `\n mensagem "${payload.message}" \n Recebida de "${payload.author}" \n na data "${payload.time}"`
+    );
+  }
+
+  @SubscribeMessage("joined-room")
+  handleJoinedRoom(client: Socket, payload) {
+    this.logger.log(`${payload.author} entrou no chat`);
+    client.join(client.id);
+  }
 }
